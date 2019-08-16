97b44e08e712f1b2b5d9d7b8954d02c7bedaac35
John Chen <johnchen456@gmail.com>
2019-08-16 16:06:01 -0400

    test: added in rubocop ignore line length

--- molecule/default/tests/test_default.rb
+++ molecule/default/tests/test_default.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
-
 describe file('/usr/local/bin/curator') do
   it { should exist }
 end
 
 describe command('curator --version') do
-  its('stdout') {
-    should eq "curator version a3d59d361ed78c799f779b4c0c55c701aae11c27\n"
-  }
+# rubocop:disable LineLength
+  its('stdout') { should eq "curator version a3d59d361ed78c799f779b4c0c55c701aae11c27\n" }
+# rubocop:enable LineLength
   its('exit_status') { should eq 0 }
 end
