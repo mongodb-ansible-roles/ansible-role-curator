0251f98a29efdbe65cdd1989231f9dd2a6cb28b6
John Chen <johnchen456@gmail.com>
2019-08-16 15:17:49 -0400

    test: adding missing tests for curator

--- molecule/default/tests/test_default.rb
+++ molecule/default/tests/test_default.rb
@@ -1,1 +1,9 @@
 # frozen_string_literal: true
+
+describe file('/usr/local/bin/curator') do
+  it { should exist }
+end
+
+describe command('curator --version') do
+  its('exit_status') { should eq 0 }
+end
