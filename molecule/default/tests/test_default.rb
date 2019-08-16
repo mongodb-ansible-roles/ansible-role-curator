f298b71229a50f33c6755f04aca91fb8aa760f95
John Chen <johnchen456@gmail.com>
2019-08-16 16:18:55 -0400

    test: added empty line after magic comment

--- molecule/default/tests/test_default.rb
+++ molecule/default/tests/test_default.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
+
 describe file('/usr/local/bin/curator') do
   it { should exist }
 end
 
 describe command('curator --version') do
   # rubocop:disable LineLength
+
   its('stdout') { should eq "curator version a3d59d361ed78c799f779b4c0c55c701aae11c27\n" }
   # rubocop:enable LineLength
+
   its('exit_status') { should eq 0 }
 end
